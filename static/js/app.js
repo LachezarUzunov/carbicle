44e116e47b63134a6fa6d5de445200444ab23995
lachezaruzunov <uzunov_89_lacho@abv.bg>
2022-02-04 08:51:46 +0200

    Adding Front-End Code

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,26 @@
-// document.getElementById('cars').addEventListener('click', ({target}) => {
-//     if (target.classList.contains('more')) {
-//         const desc = target.parentElement.querySelector('.description');
-//         if (desc.style.display == 'block') {
-//             desc.style.display = 'none';
-//             target.textContent = 'Show More';
-//         } else {
-//             desc.style.display = 'block';
-//             target.textContent = 'Hide';
-//         }
+document.getElementById("cars").addEventListener("click", (ev) => {
+  if (ev.target.classList.contains("more")) {
+    const btn = ev.target;
+    const desc = btn.parentElement.querySelector(".description");
+    if (desc.style.display == "block") {
+      desc.style.display = "none";
+      btn.textContent = "Show more";
+    } else {
+      desc.style.display = "block";
+      btn.textContent = "Hide";
+    }
+  }
+});
+
+// document.getElementById("cars").addEventListener("click", ({ target }) => {
+//   if (target.classList.contains("more")) {
+//     const desc = target.parentElement.querySelector(".description");
+//     if (desc.style.display == "block") {
+//       desc.style.display = "none";
+//       target.textContent = "Show More";
+//     } else {
+//       desc.style.display = "block";
+//       target.textContent = "Hide";
 //     }
+//   }
 // });
